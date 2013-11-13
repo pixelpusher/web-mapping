1d4dbad398f6cf2b77d506268873629cca555539
th0ran <torarne.njaamo@gmail.com>
2013-11-13 14:23:58 +0000

    Added bootstrap - Set up map

---
+++ projects/scavenger-hunt/clue-1/js/scripts.js
@@ -0,0 +1,19 @@
+ <script>
+  var map = L.map('leaflet').setView([51.505, -0.09], 15);
+  
+  var osm = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+  var toner = L.tileLayer('http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.jpg');
+  var watercolor = L.tileLayer('http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.jpg');
+
+  map.addLayer(watercolor);
+
+  window.markers = L.layerGroup().addTo(map)
+
+   map.on('click', function (evt) {
+     console.log('click', evt)
+     var marker = L.marker(evt.latlng)
+     window.markers.addLayer(marker)
+   })
+   
+  
+  </script>
