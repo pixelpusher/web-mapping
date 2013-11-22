06e84baf34a5a541d1d0d9e3b0fb8eda47f30eab
Selcuk Cura <selcukcura@gmail.com>
2013-11-22 17:10:55 +0000

    added text to display on background
    also added marker images which are not yet included

--- projects/scavenger-hunt/clue-1/js/scripts.js
+++ projects/scavenger-hunt/clue-1/js/scripts.js
@@ -1,5 +1,5 @@
 
-  var map = L.map('leaflet').setView([51.30168,0.01542], 14);
+  var map = L.map('leaflet').setView([51.5002981,0.0035886], 14);
   
   var osm = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
   var toner = L.tileLayer('http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.jpg');
@@ -9,29 +9,27 @@
 
   window.markers = L.layerGroup().addTo(map)
   
-  var clue1 = [51.30168,0.01542]
-
-
-// was originally var clue1marker = L.marker([51.502098,0.005048]); 
-//google maps station point +51°30'1.68", +0°0'15.42"
-
-  var clue1marker = L.marker([51.30168,0.01542]);
+  var clue1 = [51.5002981,0.0035886]
  
-  
+  var clue1marker = L.marker([51.5002981,0.0035886]);
  
-  var clue1Cold = L.circle(clue1,500);
+  var clue1Cold = L.circle(clue1,200);
   
-  var clue1Warm = L.circle(clue1,300,{fillColor:"#ffea00", color:"#ffea00"});
+  var clue1Warm = L.circle(clue1,100,{fillColor:"#ffea00", color:"#ffea00"});
   
-  var clue1Hot = L.circle(clue1,100,{fillColor:"#ff0000", color:"#ff0000"});
+  var clue1Warmer = L.circle(clue1,50,{fillColor:"#FA7832", color:"#FA7832"});
 
-  // window.markers.addLayer(clue1marker);
+  var clue1Hot = L.circle(clue1,5,{fillColor:"#ff0000", color:"#ff0000"});
+
+  window.markers.addLayer(clue1marker);
   
-  // window.markers.addLayer(clue1Cold);
+   window.markers.addLayer(clue1Cold);
   
-  // window.markers.addLayer(clue1Warm);
+   window.markers.addLayer(clue1Warm);
+
+   window.markers.addLayer(clue1Warmer);
   
-  // window.markers.addLayer(clue1Hot);
+   window.markers.addLayer(clue1Hot);
   
 
    map.on('click', function (evt) {
@@ -55,12 +53,14 @@
     var isCold = clue1Cold.getBounds().contains(latlng);
     
     var isWarm = clue1Warm.getBounds().contains(latlng);
+
+    var isWarmer = clue1Warm.getBounds().contains(latlng);
     
     var isHot = clue1Hot.getBounds().contains(latlng);
     
     var mapContainer = $(".map")
     
-    mapContainer.removeClass("hot warm cold")
+    mapContainer.removeClass("hot warm warmer cold")
     
     console.log("is cold",isCold);
     
@@ -68,6 +68,10 @@
 	    mapContainer.addClass("hot");
     }
     
+    else if(isWarmer) {
+	    mapContainer.addClass("warmer");
+    }
+
     else if(isWarm) {
 	    mapContainer.addClass("warm");
     }
@@ -83,5 +87,16 @@
   navigator.geolocation.watchPosition(goto); 
   
   
+  // change marker icon 
   
+  var greenIcon = L.icon({
+    iconUrl: '../images/marker.png',
+    shadowUrl: '../images/marker-shadow.png',
+
+    iconSize:     [38, 95], // size of the icon
+    shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
 
